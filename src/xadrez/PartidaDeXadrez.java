6f09d8015c0f4e070eec69246bf4abf9b88ab8aa
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-02 18:43:35 -0300

    teste

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -15,7 +15,7 @@ public class PartidaDeXadrez {
 	}
 	
 	public PecaDeXadrez[][] getPecas() { 
-		PecaDeXadrez[][] mat = new PecaDeXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
+		PecaDeXadrez[][] mat = new PecaDeXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];   
 		for(int i = 0; i < tabuleiro.getLinhas(); i++) { 
 			for(int j = 0; j < tabuleiro.getColunas(); j++) { 
 				mat[i][j] = (PecaDeXadrez) tabuleiro.peca(i, j); //fez um downcasting nessa linha
